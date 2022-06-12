13fc2bf5b232fc5eea87a5c302bcfc1dad2a2cc6
Marco <marcoxd01@hotmail.com>
2022-06-11 21:19:28 -0300

    removendo comentários e estilizando results

--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -5,7 +5,7 @@ export default function Results({ filtro }) {
   return (
     <div className="containerResults">
       <p>
-        Resultados para:{" "}
+        Resultados para:
         <span>{filtro.charAt(0).toUpperCase() + filtro.slice(1)}</span>
       </p>
     </div>
